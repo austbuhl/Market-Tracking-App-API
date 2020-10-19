fa1eddb2ec4c931768fb9e848d7df1d279d30680
Eddy_Yunhao_Gao <as510891328@gmail.com>
2020-10-19 17:34:22 -0400

    initial setup

---
+++ config/initializers/iex_client.rb
@@ -0,0 +1,8 @@
+
+
+# Configure IEX::Api.configure do |config|
+#   config.publishable_token = 'pk_6457f5f5569a457a9f95635f54b70833' # defaults to
+#   # ENV['IEX_API_PUBLISHABLE_TOKEN']
+#   config.endpoint = 'https://sandbox.iexapis.com/v1' # defaults to
+#   'https://cloud.iexapis.com/v1'
+# end
