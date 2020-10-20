bf11bb0b1c93b56bb2304ea1edc069d94ff00f81
AB <austinbuhler@gmail.com>
2020-10-20 13:04:31 -0400

    JSON formatted

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -3,23 +3,31 @@ class Api::V1::FavoritesController < ApplicationController
   def index
     favorites = Favorite.all
 
+    token = 'pk_6457f5f5569a457a9f95635f54b70833'
     client = IEX::Api::Client.new(
-      publishable_token: 'pk_6457f5f5569a457a9f95635f54b70833',
+      publishable_token: token,
       endpoint: 'https://cloud.iexapis.com/v1'
     )
 
-  
-    favorites.each do |favorite|
-      iex_quote = client.quote(favorite.stock.ticker).to_hash
-      favorite.attributes.symbolize_keys.merge({'quote' => iex_quote})
+    full_response = favorites.map do |fav|
+      response = { 
+        id: fav.id,
+        user_id: fav.user_id,
+        stock_id: fav.stock_id,
+        stock: {
+          company: fav.stock.company_name,
+          ticker: fav.stock.ticker
+        },
+        quote: client.quote(fav.stock.ticker)
+      }
     end
 
-    # render json: quote
+    render json: full_response
 
-    render json: favorites.to_json(:include => {
-      :stock => {:only => [:company_name, :ticker]},
-      :user => {:only => [:name]}     
-    })
+    # render json: favorites.to_json(:include => {
+    #   :stock => {:only => [:company_name, :ticker]},
+    #   :user => {:only => [:name]}     
+    # })
 
     
   end
